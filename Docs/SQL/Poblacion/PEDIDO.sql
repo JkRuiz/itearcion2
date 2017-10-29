17b2bd6e082ffce98a608729fefb205a835f7475
Jorge Gomez <jandresgomez@Jorges-MacBook-Pro-2.local>
2017-10-28 21:47:53 -0500

    Docs and SQL

---
+++ Docs/SQL/Poblacion/PEDIDO.sql
@@ -0,0 +1,10 @@
+insert into PEDIDO (num_pedido, precio, fecha, email_user, pagado, entregado, hora, cambio) values (1, 3119, TO_DATE('23/04/2017', 'DD/MM/YYYY'), 'elfulano@gmail.com', 0, 1, '10:30:00 PM', 'Ninguno');
+insert into PEDIDO (num_pedido, precio, fecha, email_user, pagado, entregado, hora, cambio) values (2, 5553, TO_DATE('22/01/2017', 'DD/MM/YYYY'), 'elfulano@gmail.com', 1, 1, '12:30:02 AM', 'Ninguno');
+insert into PEDIDO (num_pedido, precio, fecha, email_user, pagado, entregado, hora, cambio) values (3, 9816, TO_DATE('21/01/2017', 'DD/MM/YYYY'), 'elfulano@gmail.com', 0, 0, '08:40:09 PM', 'Ninguno');
+insert into PEDIDO (num_pedido, precio, fecha, email_user, pagado, entregado, hora, cambio) values (4, 1434, TO_DATE('20/02/2017', 'DD/MM/YYYY'), 'elfulano@gmail.com', 0, 1, '11:30:02 PM', 'Ninguno');
+insert into PEDIDO (num_pedido, precio, fecha, email_user, pagado, entregado, hora, cambio) values (5, 1269, TO_DATE('10/03/2017', 'DD/MM/YYYY'), 'ElCrack@gmail', 0, 1, '10:00:00 PM', 'Ninguno');
+insert into PEDIDO (num_pedido, precio, fecha, email_user, pagado, entregado, hora, cambio) values (6, 4821, TO_DATE('15/09/2017', 'DD/MM/YYYY'), 'elfulano@gmail.com', 0, 0, '04:50:00 PM', 'Ninguno');
+insert into PEDIDO (num_pedido, precio, fecha, email_user, pagado, entregado, hora, cambio) values (7, 6251, TO_DATE('13/11/2017', 'DD/MM/YYYY'), 'elfulano@gmail.com', 1, 0, '02:10:10 AM', 'Ninguno');
+insert into PEDIDO (num_pedido, precio, fecha, email_user, pagado, entregado, hora, cambio) values (8, 2572, TO_DATE('28/12/2017', 'DD/MM/YYYY'), 'elfulano@gmail.com', 0, 1, '01:25:02 AM', 'Ninguno');
+insert into PEDIDO (num_pedido, precio, fecha, email_user, pagado, entregado, hora, cambio) values (9, 9275, TO_DATE('30/05/2017', 'DD/MM/YYYY'), 'ElCrack@gmail', 0, 0, '10:20:00 PM', 'Ninguno');
+insert into PEDIDO (num_pedido, precio, fecha, email_user, pagado, entregado, hora, cambio) values (10, 7384, TO_DATE('23/04/2017', 'DD/MM/YYYY'), 'elfulano@gmail.com', 1, 0, '12:43:12 AM', 'Ninguno');
